12c1a21c90d2888119c38b39763a77bc8210fcc7
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2025-06-08 22:18:02 -0300

    feat(gumtea): add support for configuring FPS
    Signed-off-by: Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>

--- internal/gumtea/options.go
+++ internal/gumtea/options.go
@@ -4,10 +4,15 @@ import tea "github.com/charmbracelet/bubbletea"
 
 // Options defines configurable options for Bubble Tea programs.
 type Options struct {
+	// FPS sets the frames per second; if 0, default FPS is used.
+	FPS int `help:"Set the frames per second" env:"FPS" default:"0"`
 }
 
 // toProgramOptions converts Options into a slice of tea.ProgramOption.
 func (o *Options) toProgramOptions() []tea.ProgramOption {
 	opts := []tea.ProgramOption{}
+	if o.FPS > 0 {
+		opts = append(opts, tea.WithFPS(o.FPS))
+	}
 	return opts
 }
