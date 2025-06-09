e1dd8a7f12bbfcdf68fff96e5d05442459861a31
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2025-06-08 22:19:46 -0300

    feat(gumtea): support alternate screen mode
    Signed-off-by: Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>

--- internal/gumtea/options.go
+++ internal/gumtea/options.go
@@ -6,6 +6,8 @@ import tea "github.com/charmbracelet/bubbletea"
 type Options struct {
 	// FPS sets the frames per second; if 0, default FPS is used.
 	FPS int `help:"Set the frames per second" env:"FPS" default:"0"`
+	// Alternate enables alternate screen mode, useful for full-screen applications.
+	Alternate bool `help:"Use alternate screen mode" env:"ALTERNATE" default:"false"`
 }
 
 // toProgramOptions converts Options into a slice of tea.ProgramOption.
@@ -13,6 +15,9 @@ func (o *Options) toProgramOptions() []tea.ProgramOption {
 	opts := []tea.ProgramOption{}
 	if o.FPS > 0 {
 		opts = append(opts, tea.WithFPS(o.FPS))
+	}
+	if o.Alternate {
+		opts = append(opts, tea.WithAltScreen())
 	}
 	return opts
 }
